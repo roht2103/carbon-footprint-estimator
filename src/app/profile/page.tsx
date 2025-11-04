7ae73c6d4a58533440a4a7c3f86f0d7f80658ae0
Rohit Thorat <202402070041@mitaoe.ac.in>
2025-11-05 00:38:16 +0530

    fix: add .npmrc to install optional deps and fix profile useEffect loop

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -39,7 +39,8 @@ export default function Profile() {
 
   useEffect(() => {
     fetchProfile();
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const fetchProfile = async () => {
     try {
